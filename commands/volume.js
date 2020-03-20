d11e642d5b3a2dba33b7184b95b7af715bc90bbf
Mert Aslan <aslan-mert99@hotmail.com>
2020-03-20 20:11:43 +0300

    Fix args in volume

--- commands/volume.js
+++ commands/volume.js
@@ -8,10 +8,10 @@ exports.run = (client, message, args, ops) => {
     if (message.member.voiceChannel !== message.guild.me.voiceChannel)
         return message.channel.send('You have to be in the same voice channel as the bot');
 
-    if(isNaN(args[1]) || args[1] > 200 || args[1] < 0)
+    if(isNaN(args[0]) || args[0] > 200 || args[0] < 0)
         return message.channel.send('Input not valid. Please enter a value between 0-200');
     
-    fetched.dispatcher.setVolume(args[1]/100);
+    fetched.dispatcher.setVolume(args[0]/100);
 
-    message.channel.send(`Set the volume to ${args[1]}`);
+    message.channel.send(`Set the volume to ${args[0]}`);
 }
