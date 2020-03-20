a941a20be0b20a960de985ac5ce3acb294893efd
unknown <aslan-mert99@hotmail.com>
2020-03-20 18:01:39 +0300

    Fix undefined queue length error

--- commands/clearqueue.js
+++ commands/clearqueue.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args, ops) => {
 
     let fetched = ops.active.get(message.guild.id);
 
-    if (fetched.queue.length <= 0)
+    if (!fetched.queue)
         return message.channel.send('Queue is already empty.');
 
     let activeSong = fetched.queue[0];
