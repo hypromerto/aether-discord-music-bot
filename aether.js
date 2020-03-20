592ec2ea62ef2a1ef82dc2c46d80f463d04cf913
unknown <aslan-mert99@hotmail.com>
2020-03-20 18:42:02 +0300

    Add volume command to aether.js

--- aether.js
+++ aether.js
@@ -3,8 +3,9 @@ const discordOpus = require('@discordjs/opus');
 
 const client = new Discord.Client();
 
-const prefix = '!';
+const prefix = '/';
 const token = process.env.token;
+
 const playCommand = require('./commands/play');
 const leaveCommand = require('./commands/leave');
 const queueCommand = require('./commands/queue');
@@ -12,6 +13,7 @@ const skipCommand = require('./commands/skip');
 const pauseCommand = require('./commands/pause');
 const resumeCommand = require('./commands/resume');
 const clearqueueCommand = require('./commands/clearqueue');
+const volumeCommand = require('./commands/volume');
 
 const active = new Map();
 
@@ -32,25 +34,28 @@ client.on('message', async message => {
 	}
 
   	let args = message.content.split(" ");
-	if (args[0] === '!play')
+	if (args[0] === '/play')
 		playCommand.run(client, message, args, ops);
 
-	if (args[0] == '!queue')
+	if (args[0] == '/queue')
 		queueCommand.run(client, message, args, ops);
 	
-	if(args[0] == '!leave')
+	if(args[0] == '/leave')
 		leaveCommand.run(client, message, args, ops);
 
-	if(args[0] == '!skip')
+	if(args[0] == '/skip')
 		skipCommand.run(client, message, args, ops);
 
-	if(args[0] == '!pause')
+	if(args[0] == '/pause')
 		pauseCommand.run(client, message, args, ops);
 	
-	if(args[0] == '!resume')
+	if(args[0] == '/resume')
 		resumeCommand.run(client, message, args, ops);
 
-	if(args[0] == '!clearqueue')
+	if(args[0] == '/clearqueue')
 		clearqueueCommand.run(client, message, args, ops);
+
+	if(args[0] == '/volume')
+		volumeCommand.run(client, message, args, ops);
 		
 });
