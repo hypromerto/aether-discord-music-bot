4e2d84165b2c4a64eb2379aac2c8e97e93a78e19
Mert Aslan <aslan-mert99@hotmail.com>
2020-03-21 23:25:33 +0300

    Fix help command output

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,6 @@
 exports.run = async (client, message, args, ops) => {
 
-    let response = `__**Available Commands**__\n**/play** Usage: /play song-title OR /play youtube-url\n**/pause** Pauses the current song\n
-                    **/resume** Resumes the current paused song\n**/queue** Displays the song queue\n**/clearqueue** Clears the song queue\n
-                    **/leave** Bot leaves the voice channel.`;
+    let response = `__**Available Commands**__\n**/play** Usage: /play song-title OR /play youtube-url\n**/pause** Pauses the current song\n**/resume** Resumes the current paused song\n**/queue** Displays the song queue\n**/clearqueue** Clears the song queue\n**/leave** Bot leaves the voice channel.`;
 
     return message.channel.send(response);
 }
