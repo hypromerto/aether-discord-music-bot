4da4e5bea53eb6d6df8af75d49aade0aede397b9
Mert Aslan <aslan-mert99@hotmail.com>
2020-03-22 14:55:11 +0300

    Fix help message

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, ops) => {
 
-    let response = `__**Available Commands**__\n**/play** Usage: /play song-title OR /play youtube-url\n**/pause** Pauses the current song\n**/resume** Resumes the current paused song\n**/queue** Displays the song queue\n**/skip** Skips the current song\n**/volume "value" Changes the volume of Aether. Value needs to be between 0-200\n**/clearqueue** Clears the song queue\n**/leave** Bot leaves the voice channel.`;
+    let response = `__**Available Commands**__\n**/play** -> Usage: /play song-title OR /play youtube-url\n**/pause** -> Pauses the current song\n**/resume** -> Resumes the current paused song\n**/queue** -> Displays the song queue\n**/skip** -> Skips the current song\n**/volume value** -> Changes the volume of Aether. Value needs to be between 0-200\n**/clearqueue** -> Clears the song queue\n**/leave** -> Bot leaves the voice channel.`;
 
     return message.channel.send(response);
 }
